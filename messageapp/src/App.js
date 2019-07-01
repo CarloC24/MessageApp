9284c0a6ff61cda9f39eb8c315cf1b701790b080
CarloC24 <carlo.clamucha@gmail.com>
2019-06-30 22:26:12 -0500

    starting to try to make a room on the zoom api

--- messageapp/src/App.js
+++ messageapp/src/App.js
@@ -4,14 +4,20 @@ import "./App.css";
 import io from "socket.io-client";
 
 function App() {
+  // on component mount connect to our websocket!
   useEffect(() => {
     console.log("socket connecting");
-    ///init our socket
+    /*
+    init our socket
+    socket will trigger socket.on("connection") on the backend!
+    */
     const socket = io.connect("http://localhost:5000");
-    // connect!!!
+    // this is the socket.emit("news on the backend!")
     socket.on("news", function(data) {
       console.log(data, "receiving data");
     });
+
+    //trying out to make a zoom room api
   });
   return (
     <div className="App">
